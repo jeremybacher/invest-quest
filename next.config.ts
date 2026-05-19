138850b5300476519c30a19b6985a8a9f9e8c388
jeremybacher <bacherjere@gmail.com>
2026-05-19 19:33:38 -0300

    fix: docs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      { source: "/docs", destination: "/docs.html" },
+      { source: "/foda", destination: "/foda.html" },
+    ];
+  },
 };
 
 export default nextConfig;
